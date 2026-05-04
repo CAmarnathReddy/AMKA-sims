4b1d063e8b750afb30529907ec3cab3d590a53e8
CAmarnathReddy <129032360+CAmarnathReddy@users.noreply.github.com>
2026-05-04 13:58:40 +0530

    Initial commit

---
+++ amkasims/src/test/java/com/amka/sims/AmkasimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amka.sims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmkasimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
